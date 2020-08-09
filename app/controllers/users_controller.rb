f4763c348e068d16180ec4f5e71d080b4ae21188
shogo2421 <2421falsecarp@gmail.com>
2020-08-09 22:40:06 +0900

    createアクション作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,16 @@
-class UserController < ApplicationController
+class UsersController < ApplicationController
 
 def new
     @user = User.new
+end
+
+def create
+    User.create(user_params)
+end
+
+private
+def user_params
+    params.require(:user).permit(:name, :age)
 end
 
 
