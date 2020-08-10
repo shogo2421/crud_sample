2c75328fee1ed3a3a34793eb46db70927a034284
shogo2421 <2421falsecarp@gmail.com>
2020-08-10 17:28:44 +0900

    edit,updateアクション作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,15 @@ def create
     User.create(user_params)
 end
 
+def edit
+    @user = User.find(params[:id])
+end
+
+def update
+    user = User.find(params[:id])
+    user.update(user_params)
+end
+
 def destroy
     user = User.find(params[:id])
     user.destroy
